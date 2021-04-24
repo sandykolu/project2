b94926b3ff2174dc8e76ff79915c623a7d8c5faf
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-04-23 19:29:45 -0700

    update index.js

--- project2-Brie/flask_working/frontend/static/js/index.js
+++ project2-Brie/flask_working/frontend/static/js/index.js
@@ -1,16 +1,10 @@
-// mongo import data
-d3.json("http://localhost:5000/api/mongo").then(function(geo) {
-    console.log("mongo endpoint")
-    console.log(geo)
-})
-
 // bar chart of top 10 states by casualties
 d3.json("http://localhost:5000/api/top10").then(function(top10){
  
     console.log(top10)
 
     var layout = {
-        title: "U.S. States with Highest Tornado Casualties (2010 - 2015)",
+        title: "States with Highest Tornado Casualties",
         xaxis: {title: "States"},
         yaxis: {title: "Number of Casualties"}
 
